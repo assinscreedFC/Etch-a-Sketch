c94ae2db5981722e195bc75f98d730b8c21b112a
anis <anishammouche50@gmail.com>
2024-02-16 17:30:21 +0000

    deuxieme commit

--- java.js
+++ java.js
@@ -5,13 +5,12 @@ const couleur=document.querySelector("#random");
 const monbtn=document.querySelector("#toggle");
 const sup=document.querySelector("#supprimer");
 const container=document.querySelector(".container");
-let j=10 ;//prompt( "Enter the number of rows: ")*1;
-//const divFlex.innerHTML=`<div class="flex1"> </div>`;
+let j=10 ;
+
 const divFlex=document.createElement("div");
 divFlex.classList.add("flex1");
 
 let nodeliste=[];
-//function pour cree les petit carre dans le grand carre central
 (function (){
         for(let i=0;i<j;i++){
            container.innerHTML+=     ` <div class="flex1"></div>`;
@@ -33,7 +32,11 @@ couleur.addEventListener( 'click', ()=> {
 monbtn.addEventListener( 'click', ()=> {
     vrai=!vrai;
 });
-sup.addEventListener( 'click', ()=> {
+sup.addEventListener( 'click', supprimer )
+
+
+
+function supprimer() {
     container.innerHTML="";
     
     
@@ -46,9 +49,9 @@ row.forEach((Element)=>  Element.style.height = (100 / j) + "%" ) ;
 for(let k=0;k<row.length;k++) {
 row.forEach((Element)=>  Element.innerHTML+=` <div id="${k}" style="width: ${(100 / j) + "%"};"> </div>`  ) ;
 
+    
 }
-
-});
+}
 
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
@@ -58,28 +61,28 @@ function getRandomColor() {
     }
     return couleur;
 }
-
+const blt=document.querySelector(".mapute")
 let color="#000000";
 inputCouleur = document.getElementById('choixCouleur');
 inputCouleur.addEventListener('input', function() {
-    // Récupérez la valeur de la couleur choisie
      color = inputCouleur.value;
-const blt=document.querySelector(".mapute")
-    blt.style.boxShadow =`0.5px 1.5px 25px 7.5px ${color}`;
+
+   
      } );
 
 addEventListener("pointerover", (event) => {
      if(vrai){
         
-         var parentElement = event.target.parentNode;
+         let parentElement = event.target.parentNode;
     parentElement=parentElement.parentNode;
-    // Vérifiez si l'élément parent a la classe spécifique "container"
  
      
      if(parentElement.classList.contains("container")&& !event.target.style.backgroundColor ){
      
      if(colorvrai){
-        event.target.style.backgroundColor =getRandomColor();
+         event.target.style.backgroundColor =getRandomColor();
+       
+        
      }else{
        event.target.style.backgroundColor =color;}}
     
@@ -90,28 +93,46 @@ addEventListener("pointerover", (event) => {
 const elementAAnimer=document.querySelector("h1");
 
 window.addEventListener('blur', function() {
-    // Vous avez quitté l'onglet, désactivez l'animation
     elementAAnimer.classList.remove('animate__animated', 'animate__backInDown');
     
 });
 
-// Ajoutez un gestionnaire d'événements pour l'événement focus
 window.addEventListener('focus', function() {
-    // Vous êtes revenu dans l'onglet, activez l'animation
     
     elementAAnimer.classList.add('animate__animated', 'animate__backInDown');
     
 });
 
-// onpointerover = (event) => event.style.color="red";
-(function(){ 
+const range = document.querySelector("#range");
+const label = document.querySelector(".price label");
+const boxPrices = document.querySelectorAll(".box");
 
+let value = 16;
+range.value = value;
 
+let you=vrai;
+function updateRange() {
+  let percentage = (100 * value) / 100;
+  range.style.background = ` linear-gradient(90deg, rgba(117,20,241,1) 0%, rgba(175,26,170,1) ${percentage}%, rgba(233,61,90,1) ${percentage}%, rgba(62,48,56,1) ${percentage}%)`;
+  label.innerText =  ` ${value} x ${value}` ;
 
+  range.addEventListener("input", (e) => {
+    j = percentage = (100 * value) / 100;
+  
+    value = +e.target.value;
+    range.style.background = ` linear-gradient(90deg, rgba(117,20,241,1) 0%, rgba(175,26,170,1) ${percentage}%, rgba(233,61,90,1) ${percentage}%, rgba(62,48,56,1) ${percentage}%)`;
+    label.innerText = ` ${value} x ${value} `;
+    
+    });
+   
+    
+    
+  }
+  updateRange();
+  
 
 
 
-})();//creation de la methode qui ajoute des elements a l'interieur
 
 
 
@@ -148,58 +169,6 @@ window.addEventListener('focus', function() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const flex = document.querySelectorAll('.flex1');
-// console.log(flex);
-
-// let j = prompt("Entrer un nombre") * 1;
-
-// (function () {
-        
-
-
-
-//     for (let i = 0; i < j; i++) {
-        
-//         const flexItem = document.createElement('div');
-        
-       
-//         flexItem.id = i;
-
-        
-//         flexItem.style.width = (100 / j) + "%";
-        
-        
-//         for (let k = 0; k < flex.length; k++) {
-//             flex[k].appendChild(flexItem.cloneNode(true));
-//         }
-//     }
-// })();
-// (function(){
-// for(let i=0;i<j;i++){
-//     let elements = document.getElementById(`${i}`);
-// console.log(elements);
-        
-        
-
-//            elements.style.height = (100 / j) + "%"               ;
-
-       
-// }
-
-
-// })();
 
 
 
