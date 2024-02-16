7291e3c0226235e1cc1ef98f0e6a286ce7e5ae84
anis <anishammouche50@gmail.com>
2024-02-16 22:42:39 +0000

    last change

--- java.js
+++ java.js
@@ -5,54 +5,62 @@ const couleur=document.querySelector("#random");
 const monbtn=document.querySelector("#toggle");
 const sup=document.querySelector("#supprimer");
 const container=document.querySelector(".container");
-let j=10 ;//prompt( "Enter the number of rows: ")*1;
-//const divFlex.innerHTML=`<div class="flex1"> </div>`;
+const range = document.querySelector("#range");
+const label = document.querySelector(".price label");
 const divFlex=document.createElement("div");
+const blt=document.querySelector(".mapute");
+const elementAAnimer=document.querySelector("h1");
+
 divFlex.classList.add("flex1");
 
+let color="#000000";
+let j=10 ;
+let value = 16;
 let nodeliste=[];
-//function pour cree les petit carre dans le grand carre central
-(function (){
-        for(let i=0;i<j;i++){
-           container.innerHTML+=     ` <div class="flex1"></div>`;
-        }
-const  row=document.querySelectorAll(".flex1");
+inputCouleur = document.getElementById('choixCouleur');
+inputCouleur.oninput= (e)=> color=e.target.value
+couleur.onclick = ()=> colorvrai=!colorvrai
+monbtn.onclick = ()=> vrai=!vrai
+sup.onclick=() => reloadGrid()
+range.onmousemove = (e) => updateSizeValue(e.target.value)
+range.onchange = (e) => changeSize(e.target.value)
+let percentage = (100 * value) / 100;
+range.style.background = `linear-gradient(90deg, rgba(117,20,241,1) 0%, rgba(175,26,170,1) ${percentage}%, rgba(233,61,90,1) ${percentage}%, rgba(62,48,56,1) ${percentage}%)`;
 
-row.forEach((Element)=>  Element.style.height = (100 / j) + "%" ) ;
-for(let k=0;k<row.length;k++) {
-row.forEach((Element)=>  Element.innerHTML+=` <div id="${k}" style="width: ${(100 / j) + "%"};"> </div>`  ) ;
+function changeSize(value) {
+    setCurrentSize(value)
+    updateSizeValue(value)
+    reloadGrid()
+  }
 
-}
-
-})();
-
-couleur.addEventListener( 'click', ()=> {
-    colorvrai=!colorvrai;
-});
-
-monbtn.addEventListener( 'click', ()=> {
-    vrai=!vrai;
-});
-sup.addEventListener( 'click', supprimer )
-
-
-
-function supprimer() {
-    container.innerHTML="";
-    
-    
+function setCurrentSize(newSize) {
+    j = newSize
+  }
+function updateSizeValue(value) {
+    percentage = (100 * value) / 100;
+    range.style.background = `linear-gradient(90deg, rgba(117,20,241,1) 0%, rgba(175,26,170,1) ${percentage}%, rgba(233,61,90,1) ${percentage}%, rgba(62,48,56,1) ${percentage}%)`;
+    label.innerText = ` ${value} x ${value}`;
+  }
+function reloadGrid() {
+    clearGrid()
+    setupGrid()
+  }
+function clearGrid() {
+    container.innerHTML = ''
+  }
+function setupGrid(){
     for(let i=0;i<j;i++){
-        container.innerHTML+=     ` <div class="flex1"></div>`;
-     }
+       container.innerHTML+=     ` <div class="flex1"></div>`;
+    }
 const  row=document.querySelectorAll(".flex1");
 
 row.forEach((Element)=>  Element.style.height = (100 / j) + "%" ) ;
 for(let k=0;k<row.length;k++) {
 row.forEach((Element)=>  Element.innerHTML+=` <div id="${k}" style="width: ${(100 / j) + "%"};"> </div>`  ) ;
 
-    
 }
-}
+};
+
 
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
@@ -62,75 +70,29 @@ function getRandomColor() {
     }
     return couleur;
 }
-const blt=document.querySelector(".mapute")
-let color="#000000";
-inputCouleur = document.getElementById('choixCouleur');
-inputCouleur.addEventListener('input', function() {
-    // Récupérez la valeur de la couleur choisie
-     color = inputCouleur.value;
-
-    blt.style.boxShadow =`0.5px 1.5px 25px 7.5px ${color}`;
-     } );
 
 addEventListener("pointerover", (event) => {
-     if(vrai){
+    if(vrai){
         
-         var parentElement = event.target.parentNode;
+    let parentElement = event.target.parentNode;
     parentElement=parentElement.parentNode;
-    // Vérifiez si l'élément parent a la classe spécifique "container"
- 
-     
      if(parentElement.classList.contains("container")&& !event.target.style.backgroundColor ){
-     
-     if(colorvrai){
-         event.target.style.backgroundColor =getRandomColor();
-       
         
-     }else{
-       event.target.style.backgroundColor =color;}}
-    
-    }
-    inputCouleur = document.getElementById('choixCouleur');
-   
-});
-const elementAAnimer=document.querySelector("h1");
+        if(colorvrai){
+            event.target.style.backgroundColor =getRandomColor();
+        }else{
+        event.target.style.backgroundColor =color;}}
+        }
+        inputCouleur = document.getElementById('choixCouleur');
+    });
 
 window.addEventListener('blur', function() {
-    // Vous avez quitté l'onglet, désactivez l'animation
     elementAAnimer.classList.remove('animate__animated', 'animate__backInDown');
-    
 });
-
-// Ajoutez un gestionnaire d'événements pour l'événement focus
 window.addEventListener('focus', function() {
-    // Vous êtes revenu dans l'onglet, activez l'animation
-    
     elementAAnimer.classList.add('animate__animated', 'animate__backInDown');
-    
 });
-
-const range = document.querySelector("#range");
-const label = document.querySelector(".price label");
-
-
-let value = 16;
-range.value = value;
-
-
- 
-    let percentage = (100 * value) / 100;
-    range.style.background = `linear-gradient(90deg, rgba(117,20,241,1) 0%, rgba(175,26,170,1) ${percentage}%, rgba(233,61,90,1) ${percentage}%, rgba(62,48,56,1) ${percentage}%)`;
-    label.innerText = ` ${value} x ${value}`;
-
-    range.addEventListener("input", (e) => {
-      j = percentage = (100 * value) / 100;
-      value = +e.target.value;
-      range.style.background = `linear-gradient(90deg, rgba(117,20,241,1) 0%, rgba(175,26,170,1) ${percentage}%, rgba(233,61,90,1) ${percentage}%, rgba(62,48,56,1) ${percentage}%)`;
-      label.innerText = ` ${value} x ${value}`;
-   
-
-   
-  });
-
-
-
+ window.onload = () => {
+   setupGrid()
+    
+  }
